a3f28953e0e9c5b0fbbeb6c671a8ffc3f067378f
Jamshed <juraev@bankplanet9.com>
2025-05-27 17:07:37 +0500

    added config

--- domain/provider/loan.go
+++ domain/provider/loan.go
@@ -3,7 +3,9 @@ package provider
 import "context"
 
 type LoanProviderI interface {
+	// LoanProduct
 	CreateLoanProduct(ctx context.Context, in CreateLoanProductIn) error
+	// Loan
 	CreateLoan(ctx context.Context, in CreateLoanIn) error
 	ActivateLoan(ctx context.Context, id string) error
 	IssueLoan(ctx context.Context, id string) error
--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/JamshedJ/loanly/config"
+	v1 "github.com/JamshedJ/loanly/delivery/rest/admin/v1"
+	"github.com/JamshedJ/loanly/domain/services"
+	"github.com/rs/zerolog"
+)
+
 func main() {
-	
+	logger := zerolog.New(os.Stdout).With().Timestamp().Logger()
+
+	cfg := config.InitConfig("./config")
+
+	svc := services.ServiceFacade{}
+
+	adminApi := v1.AdminApiV1{
+		Logger: logger,
+		Svc:    svc,
+	}
+
+	adminApi.RegisterRoutes().Run(fmt.Sprintf(":%d", cfg.App.Port))
 }
